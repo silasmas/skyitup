e7699ac0177b8836104690c9ac581a7f2f53bbdb
xanderssamoth <bolulathomas@yahoo.fr>
2022-12-20 15:12:33 +0100

    u

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -68,7 +68,7 @@ Customizer : Xanders Samoth | https://www.linkedin.com/in/xanders-samoth-b277073
             nav: true,
             items: 1,
             smartSpeed: 800,
-            navText: ['<i><img src="assets/images/angle-left.png" alt="icon"/></i><span>prev</span>', '<span>next</span><i><img src="assets/images/angle-right.png" alt="icon"/></i>']
+            navText: ['<i><img src="assets/img/angle-left.png" alt="icon"/></i><span>prev</span>', '<span>next</span><i><img src="assets/img/angle-right.png" alt="icon"/></i>']
         });
     };
     slider_area();
